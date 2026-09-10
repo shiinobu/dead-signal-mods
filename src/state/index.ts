651689de86f71c2867abc7946c37de8dea9ccd88
Firman Aprilian Sugiharto <55623461+shiinobu@users.noreply.github.com>
2026-09-11 01:21:53 +0700

    feat(persistence): export state serializer

--- src/state/index.ts
+++ src/state/index.ts
@@ -4,3 +4,4 @@ export * from "./flag-store.js";
 export * from "./state-store.js";
 export * from "./domain-state-access.js";
 export * from "./persistence.js";
+export * from "./state-serializer.js";
