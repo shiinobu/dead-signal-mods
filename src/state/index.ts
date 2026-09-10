2e475de674a2a3e61c42f74843e096ed7e03afe7
Firman Aprilian Sugiharto <55623461+shiinobu@users.noreply.github.com>
2026-09-11 01:25:36 +0700

    feat(persistence): export state validator

--- src/state/index.ts
+++ src/state/index.ts
@@ -5,3 +5,4 @@ export * from "./state-store.js";
 export * from "./domain-state-access.js";
 export * from "./persistence.js";
 export * from "./state-serializer.js";
+export * from "./state-validator.js";
