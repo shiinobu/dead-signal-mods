ca6a20faea92afb2ed6cbc2cb24fcc595bae4253
Firman Aprilian Sugiharto <55623461+shiinobu@users.noreply.github.com>
2026-09-11 15:12:33 +0700

    chore(hackhub): remove phase12 diagnostics from production bootstrap

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,10 @@ import {
     RegisterModPackage,
 } from "@hotbunny/hackhub-content-sdk";
 
-import { runPhase12RuntimeRegression } from "./infrastructure/hackhub/phase12-runtime-regression.js";
-import { prepareNmapSmokeTest } from "./infrastructure/hackhub/dead-signal-nmap-smoke-quest.js";
-import "./infrastructure/hackhub/dead-signal-smoke-quest.js";
-
 @RegisterModPackage
 export default class DeadSignalMod extends Bootstrap {
     override OnModPackageLoaded() {
         console.log("DEAD SIGNAL mod loaded!");
-        prepareNmapSmokeTest();
-        runPhase12RuntimeRegression();
     }
 
     override OnModPackageUnloaded() {
