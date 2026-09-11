d10205bf492fa5fc94c67feaf4e6a387880c51f2
Firman Aprilian Sugiharto <55623461+shiinobu@users.noreply.github.com>
2026-09-11 14:14:41 +0700

    feat(phase12): wire HackHub SaveStorage integration smoke test

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import {
     RegisterModPackage,
 } from "@hotbunny/hackhub-content-sdk";
 
+import { runSaveStorageSmokeTest } from "./infrastructure/hackhub/save-storage-smoke-test.js";
 import "./infrastructure/hackhub/dead-signal-smoke-quest.js";
 
 @RegisterModPackage
 export default class DeadSignalMod extends Bootstrap {
     override OnModPackageLoaded() {
         console.log("DEAD SIGNAL mod loaded!");
+        runSaveStorageSmokeTest();
     }
 
     override OnModPackageUnloaded() {
