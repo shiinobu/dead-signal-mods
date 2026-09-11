66256f660fe1d8e99efb494d2595c993fcf37b7c
Firman Aprilian Sugiharto <55623461+shiinobu@users.noreply.github.com>
2026-09-11 14:40:26 +0700

    test(hackhub): run phase12 full runtime regression

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,14 @@ import {
     RegisterModPackage,
 } from "@hotbunny/hackhub-content-sdk";
 
-import { runEndingSmokeTest } from "./infrastructure/hackhub/ending-smoke-test.js";
-import { runRuntimeServicesSmokeTest } from "./infrastructure/hackhub/runtime-services-smoke-test.js";
-import { runSaveStorageSmokeTest } from "./infrastructure/hackhub/save-storage-smoke-test.js";
+import { runPhase12RuntimeRegression } from "./infrastructure/hackhub/phase12-runtime-regression.js";
 import "./infrastructure/hackhub/dead-signal-smoke-quest.js";
 
 @RegisterModPackage
 export default class DeadSignalMod extends Bootstrap {
     override OnModPackageLoaded() {
         console.log("DEAD SIGNAL mod loaded!");
-        runSaveStorageSmokeTest();
-        runRuntimeServicesSmokeTest();
-        runEndingSmokeTest();
+        runPhase12RuntimeRegression();
     }
 
     override OnModPackageUnloaded() {
