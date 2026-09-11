1999833b5dbe2ffc16167e5168da2c2657c38061
Firman Aprilian Sugiharto <55623461+shiinobu@users.noreply.github.com>
2026-09-11 14:33:39 +0700

    test(hackhub): run phase12 ending integration smoke

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {
     RegisterModPackage,
 } from "@hotbunny/hackhub-content-sdk";
 
+import { runEndingSmokeTest } from "./infrastructure/hackhub/ending-smoke-test.js";
 import { runRuntimeServicesSmokeTest } from "./infrastructure/hackhub/runtime-services-smoke-test.js";
 import { runSaveStorageSmokeTest } from "./infrastructure/hackhub/save-storage-smoke-test.js";
 import "./infrastructure/hackhub/dead-signal-smoke-quest.js";
@@ -13,6 +14,7 @@ export default class DeadSignalMod extends Bootstrap {
         console.log("DEAD SIGNAL mod loaded!");
         runSaveStorageSmokeTest();
         runRuntimeServicesSmokeTest();
+        runEndingSmokeTest();
     }
 
     override OnModPackageUnloaded() {
